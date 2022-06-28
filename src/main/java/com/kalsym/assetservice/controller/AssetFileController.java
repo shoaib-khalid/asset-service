8e44954cff8091cded25a5490a2555a57f30ed91
nur.iman <nur.iman@kalsym.com>
2022-06-28 15:24:47 +0800

    comment unused code

--- src/main/java/com/kalsym/assetservice/controller/AssetFileController.java
+++ src/main/java/com/kalsym/assetservice/controller/AssetFileController.java
@@ -69,146 +69,146 @@ public class AssetFileController {
     }
 
     /// Get file asset by file path (encoded uri)
-    @GetMapping(path = {"/file"})
-    public ResponseEntity<HttpResponse> getFileAssetByDirectoryPath(
-        HttpServletRequest request,
-        @RequestParam(required = false) String asseturl
-    ) {
+    // @GetMapping(path = {"/file"})
+    // public ResponseEntity<HttpResponse> getFileAssetByDirectoryPath(
+    //     HttpServletRequest request,
+    //     @RequestParam(required = false) String asseturl
+    // ) {
 
-        String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
+    //     String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
 
-        AssetFile af = assetFileService.fileDetails(fileDirectory);
+    //     AssetFile af = assetFileService.fileDetails(fileDirectory);
 
  
-        HttpResponse response = new HttpResponse(request.getRequestURI());
+    //     HttpResponse response = new HttpResponse(request.getRequestURI());
 
-        response.setData(af);
-        response.setStatus(HttpStatus.OK);
+    //     response.setData(af);
+    //     response.setStatus(HttpStatus.OK);
         
-        return ResponseEntity.status(response.getStatus()).body(response);
+    //     return ResponseEntity.status(response.getStatus()).body(response);
 
-    }
+    // }
 
     /// Get file asset by encoded uri file path 
-    @GetMapping(value = "/view")
-    public ResponseEntity<Resource> viewOriginalImage(        
-        @RequestParam(required = false) String asseturl
+    // @GetMapping(value = "/view")
+    // public ResponseEntity<Resource> viewOriginalImage(        
+    //     @RequestParam(required = false) String asseturl
 
-    ) throws IOException {
+    // ) throws IOException {
 
-        //To get file directory based on asset url
-        int k = asseturl.lastIndexOf('/');
-        String filename = asseturl.substring(k+1);
-        // String fileDirectory = filePath + "/" + filename;
+    //     //To get file directory based on asset url
+    //     int k = asseturl.lastIndexOf('/');
+    //     String filename = asseturl.substring(k+1);
+    //     // String fileDirectory = filePath + "/" + filename;
 
-        String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
+    //     String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
 
-        final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
-            fileDirectory
-        )));
+    //     final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+    //         fileDirectory
+    //     )));
         
-            //To get extension file
-            int i = filename.lastIndexOf('.');
-            String fileType = filename.substring(i+1);
+    //         //To get extension file
+    //         int i = filename.lastIndexOf('.');
+    //         String fileType = filename.substring(i+1);
 
-            MediaType mediatype ;
+    //         MediaType mediatype ;
 
-            switch (fileType){
+    //         switch (fileType){
 
-                case "png":
-                mediatype = MediaType.IMAGE_PNG;
-                break;
+    //             case "png":
+    //             mediatype = MediaType.IMAGE_PNG;
+    //             break;
 
-                case "jpg":
-                mediatype = MediaType.IMAGE_JPEG;
-                break;
+    //             case "jpg":
+    //             mediatype = MediaType.IMAGE_JPEG;
+    //             break;
 
-                case "gif":
-                mediatype = MediaType.IMAGE_GIF;
-                break;
+    //             case "gif":
+    //             mediatype = MediaType.IMAGE_GIF;
+    //             break;
 
-                case "pdf":
-                mediatype = MediaType.APPLICATION_PDF;
-                break;
+    //             case "pdf":
+    //             mediatype = MediaType.APPLICATION_PDF;
+    //             break;
 
-                default:
-                mediatype = MediaType.APPLICATION_JSON;
+    //             default:
+    //             mediatype = MediaType.APPLICATION_JSON;
 
-            }
+    //         }
 
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .contentLength(inputStream.contentLength())
-                .contentType(mediatype)
-                .body(inputStream);
+    //     return ResponseEntity
+    //             .status(HttpStatus.OK)
+    //             .contentLength(inputStream.contentLength())
+    //             .contentType(mediatype)
+    //             .body(inputStream);
 
-    }
+    // }
 
     //https://github.com/lokeshnous/migration/blob/e171df65f7c6eb59901854c3ed069973b5359c80/jobboard/src/main/java/com/advanceweb/afc/jb/home/web/controller/HomeController.java
-    @RequestMapping("/resize")
-	public ResponseEntity<byte[]> getResizeImage(
-        @RequestParam(required = false) String relativePath,
-        @RequestParam(required = false) Integer width,
-        @RequestParam(required = false) Integer height,
-			HttpServletResponse response, 
-            HttpServletRequest request) {
-		try {
+    // @RequestMapping("/resize")
+	// public ResponseEntity<byte[]> getResizeImage(
+    //     @RequestParam(required = false) String relativePath,
+    //     @RequestParam(required = false) Integer width,
+    //     @RequestParam(required = false) Integer height,
+	// 		HttpServletResponse response, 
+    //         HttpServletRequest request) {
+	// 	try {
 
-            String fileDirectory = assetFileService.getFolderFilePath(relativePath);
+    //         String fileDirectory = assetFileService.getFolderFilePath(relativePath);
 
-			BufferedImage originalImage = ImageIO.read(new File(fileDirectory));
+	// 		BufferedImage originalImage = ImageIO.read(new File(fileDirectory));
 
-            BufferedImage scaleImage = assetFileService.resizeImage(originalImage, width, height);
-            // BufferedImage scaleImage = assetFileService.scale(originalImage, width, height);
+    //         BufferedImage scaleImage = assetFileService.resizeImage(originalImage, width, height);
+    //         // BufferedImage scaleImage = assetFileService.scale(originalImage, width, height);
 
 
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(scaleImage, "jpg", baos);
-			baos.flush();
-			byte[] imageInByte = baos.toByteArray();
-			baos.close();
+    //         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	// 		ImageIO.write(scaleImage, "jpg", baos);
+	// 		baos.flush();
+	// 		byte[] imageInByte = baos.toByteArray();
+	// 		baos.close();
 
-			// ResponseEntity<byte[]> result = handleGetMyBytesRequest(imageInByte);
+	// 		// ResponseEntity<byte[]> result = handleGetMyBytesRequest(imageInByte);
 
-            HttpHeaders responseHeaders = new HttpHeaders();
-            responseHeaders.setContentType(MediaType.IMAGE_PNG);
-            responseHeaders.setContentLength(imageInByte.length);
+    //         HttpHeaders responseHeaders = new HttpHeaders();
+    //         responseHeaders.setContentType(MediaType.IMAGE_PNG);
+    //         responseHeaders.setContentLength(imageInByte.length);
     
-            return new ResponseEntity<byte[]>(imageInByte, responseHeaders,
-                    HttpStatus.OK);
+    //         return new ResponseEntity<byte[]>(imageInByte, responseHeaders,
+    //                 HttpStatus.OK);
 
-			// Display the image
-			// write(response, result.getBody());
-		} catch (Exception e) {
-            System.out.println("ERROR :::::::"+e.getMessage());
-            e.printStackTrace();
+	// 		// Display the image
+	// 		// write(response, result.getBody());
+	// 	} catch (Exception e) {
+    //         System.out.println("ERROR :::::::"+e.getMessage());
+    //         e.printStackTrace();
             
-            return new ResponseEntity<byte[]>(
-                    HttpStatus.OK);
+    //         return new ResponseEntity<byte[]>(
+    //                 HttpStatus.OK);
             
-		}
-	}
+	// 	}
+	// }
 
     /// Get All file asset  Note : asseturl (only for folder)
-    @GetMapping(path = {"/folder"})
-    public ResponseEntity<HttpResponse> getAllFilesSubFolder(
-        HttpServletRequest request,
-        @RequestParam(required = false) String asseturl
-    ) {
+    // @GetMapping(path = {"/folder"})
+    // public ResponseEntity<HttpResponse> getAllFilesSubFolder(
+    //     HttpServletRequest request,
+    //     @RequestParam(required = false) String asseturl
+    // ) {
 
-        String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
+    //     String fileDirectory = assetFileService.getFileDirectoryPath(asseturl);
 
-        List<AssetFile> fileArrayList= assetFileService.fileListing(fileDirectory);
+    //     List<AssetFile> fileArrayList= assetFileService.fileListing(fileDirectory);
 
-        HttpResponse response = new HttpResponse(request.getRequestURI());
+    //     HttpResponse response = new HttpResponse(request.getRequestURI());
 
-        response.setData(fileArrayList);
-        response.setStatus(HttpStatus.OK);
+    //     response.setData(fileArrayList);
+    //     response.setStatus(HttpStatus.OK);
         
-        return ResponseEntity.status(response.getStatus()).body(response);
+    //     return ResponseEntity.status(response.getStatus()).body(response);
 
-    }
+    // }
 
 
 }
